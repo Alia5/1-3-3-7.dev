22178d7bbdd6ad63b8c6e213fab149b5e93c6d3f
BuildTools <unconfigured@null.spigotmc.org>
2022-04-01 19:47:50 +0200

    initial commit

---
+++ src/ts/index.ts
@@ -0,0 +1,17 @@
+
+const printmotd = () => {
+    console.log(`
+    Roses are red
+    Violets are blue
+    I'll fucking kill you if
+    You write "== true"
+    `);
+};
+
+
+const main = () => {
+    printmotd();
+};
+
+
+main();
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+/**
+ * Webpack main configuration file
+ */
+
+const path = require('path');
+const fs = require('fs');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: process.env["PROD"] ? 'production' : 'development',
+    entry: [path.resolve(__dirname, './src/ts/index.ts'), path.resolve(__dirname, './src/scss/style.scss')],
+    output: {
+        filename: 'js/index.js',
+        clean: true,
+    },
+    watchOptions: {
+        poll: 1000, // Check for changes every second
+    },
+    devServer: {
+        compress: true,
+        port: 9000,
+        watchFiles: [path.join(__dirname, 'src') + "/**/*.*"],
+        hot: true,
+        liveReload: true
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: path.join(__dirname, 'src/html'), to: "." },
+                { from: path.join(__dirname, 'src/assets'), to: "assets" },
+                { from: path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'), to: "assets/fonts" }
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ],
+                include: [path.resolve(__dirname, 'src', 'ts')]
+            },
+            {
+                test: /\.(ttf|eot|svg|woff|woff2)(\?[\s\S]+)?$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                include: [path.resolve(__dirname, 'src', 'scss')],
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                            outputPath: 'css/'
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ],
+            },
+        ],
+    },
+}
