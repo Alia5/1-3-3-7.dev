bc8e95f14f1d3bfac89dd2c32d5b0888abc84023
Peter Repukat <orly.alia5@gmail.com>
2022-09-18 16:16:51 +0200

    Add diy cookie warning
    fuck eu

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,4 @@
+import * as isbot from 'isbot'
 
 const printmotd = () => {
     console.log(`
@@ -8,10 +9,46 @@ const printmotd = () => {
     `);
 };
 
+const loadGads = () => {
+    const scripts = document.getElementsByTagName("script");
+    Array.from(scripts).forEach((s) => {
+        if (s.type === "required") {
+            s.remove();
+            s.type = '';
+            s.removeAttribute('data-type');
+            s.removeAttribute('data-name');
+            document.getElementsByTagName('head')?.[0]?.appendChild(s);
+        }
+    })
+}
 
 const main = () => {
     printmotd();
+    if (window.localStorage.getItem("cookiesAccepted") !== "true") {
+        (document.querySelector('.cookieShit') as HTMLElement).style.display = "flex"  
+    } else {
+        loadGads();
+    }
+
+    if (isbot(navigator.userAgent)) {
+        console.log("isbot");
+        window.localStorage.setItem("cookiesAccepted", "true");
+        loadGads();
+    }
+
+    (document.querySelector('#darkSide') as HTMLButtonElement).onclick = () => {
+        (document.querySelector('.cookieShit') as HTMLElement).style.display = "none"
+        window.localStorage.setItem("cookiesAccepted", "true");
+        loadGads();
+    }
+    (document.querySelector('#andYouAreOut') as HTMLButtonElement).onclick = () => {
+        open('https://google.com', '_self')
+    }
 };
 
 
 main();
+function isBot() {
+    throw new Error("Function not implemented.");
+}
+
