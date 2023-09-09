ea319a7cac45279d12b419b3a2eb4f2f66784614
Peter Repukat <orly.alia5@gmail.com>
2023-09-09 16:03:04 +0200

    Update 1-3-3-7.dev

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -9,42 +9,42 @@ const printmotd = () => {
     `);
 };
 
-const loadGads = () => {
-    (document.querySelector('.cookieShit') as HTMLElement).style.display = "none"
-    const scripts = document.getElementsByTagName("script");
-    Array.from(scripts).forEach((s) => {
-        if (s.type === "required") {
-            s.remove();
-            s.type = '';
-            s.removeAttribute('data-type');
-            s.removeAttribute('data-name');
-            document.getElementsByTagName('head')?.[0]?.appendChild(s);
-        }
-    })
-}
+// const loadGads = () => {
+//     (document.querySelector('.cookieShit') as HTMLElement).style.display = "none"
+//     const scripts = document.getElementsByTagName("script");
+//     Array.from(scripts).forEach((s) => {
+//         if (s.type === "required") {
+//             s.remove();
+//             s.type = '';
+//             s.removeAttribute('data-type');
+//             s.removeAttribute('data-name');
+//             document.getElementsByTagName('head')?.[0]?.appendChild(s);
+//         }
+//     })
+// }
 
 const main = () => {
     printmotd();
 
-    if (isbot(navigator.userAgent)) {
-        console.log("isbot");
-        window.localStorage.setItem("cookiesAccepted", "true");
-        loadGads();
-    } else {
-        if (window.localStorage.getItem("cookiesAccepted") !== "true") {
-            (document.querySelector('.cookieShit') as HTMLElement).style.display = "flex"  
-        } else {
-            loadGads();
-        }
-    }
+    // if (isbot(navigator.userAgent)) {
+    //     console.log("isbot");
+    //     window.localStorage.setItem("cookiesAccepted", "true");
+    //     loadGads();
+    // } else {
+    //     if (window.localStorage.getItem("cookiesAccepted") !== "true") {
+    //         (document.querySelector('.cookieShit') as HTMLElement).style.display = "flex"  
+    //     } else {
+    //         loadGads();
+    //     }
+    // }
 
-    (document.querySelector('#darkSide') as HTMLButtonElement).onclick = () => {
-        window.localStorage.setItem("cookiesAccepted", "true");
-        loadGads();
-    }
-    (document.querySelector('#andYouAreOut') as HTMLButtonElement).onclick = () => {
-        open('https://google.com', '_self')
-    }
+    // (document.querySelector('#darkSide') as HTMLButtonElement).onclick = () => {
+    //     window.localStorage.setItem("cookiesAccepted", "true");
+    //     loadGads();
+    // }
+    // (document.querySelector('#andYouAreOut') as HTMLButtonElement).onclick = () => {
+    //     open('https://google.com', '_self')
+    // }
 };
 
 
